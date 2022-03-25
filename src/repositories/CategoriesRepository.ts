6e677eeda3d13de87d5404eca36466cea9a4562c
Vinicius Meneses <vinicius.meneses@tagview.com.br>
2022-03-24 23:20:28 -0300

    Cria service para cadastro de categoria

--- src/repositories/CategoriesRepository.ts
+++ src/repositories/CategoriesRepository.ts
@@ -13,7 +13,7 @@ class CategoriesRepository {
     this.categories = [];
   }
 
-  create({ name, description }: ICreateCategoryDTO) {
+  create({ name, description }: ICreateCategoryDTO): void {
     const category = new Category();
     Object.assign(category, { name, description, created_at: new Date() });
     this.categories.push(category);
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 
 import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { CreateCategoryService } from "../services/CreateCategoryService";
 
 const categoriesRoutes = Router();
 
@@ -9,12 +10,10 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post("/", (req, res) => {
   const { name, description } = req.body;
 
-  const categoryAlreadyExists = categoriesRepository.findByName(name);
-  if (categoryAlreadyExists)
-    return res.status(400).json({ error: "Category already exists" });
+  const createCategoryService = new CreateCategoryService(categoriesRepository);
+  createCategoryService.execute({ name, description });
 
-  categoriesRepository.create({ name, description });
-  return res.status(201);
+  return res.sendStatus(201);
 });
 
 categoriesRoutes.get("/", (req, res) => {
