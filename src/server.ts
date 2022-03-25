eb9ae3d532a3e27862e793eb36e6fcd41b50fb26
Vinicius Meneses <vinicius.meneses@tagview.com.br>
2022-03-24 22:12:38 -0300

    Inicializa projeto

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import express from "express";
+
+import { categoriesRoutes } from "./routes/categories.routes";
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(3333, () => console.log("Server started on port 3333"));
