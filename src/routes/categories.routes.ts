8fed3ef19e399839ebb17c49c86fee4883510f12
Vinicius Meneses <vinicius.meneses@tagview.com.br>
2022-03-25 22:40:28 -0300

    Separa listagem e criação de categorias em UseCases

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,24 +1,11 @@
 import { Router } from "express";
 
-import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepository";
-import { CreateCategoryService } from "../modules/cars/services/CreateCategoryService";
+import { createCategoryController } from "../modules/cars/useCases/createCategory";
+import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 const categoriesRoutes = Router();
 
-const categoriesRepository = new CategoriesRepository();
-
-categoriesRoutes.post("/", (req, res) => {
-  const { name, description } = req.body;
-
-  const createCategoryService = new CreateCategoryService(categoriesRepository);
-  createCategoryService.execute({ name, description });
-
-  return res.sendStatus(201);
-});
-
-categoriesRoutes.get("/", (req, res) => {
-  const all = categoriesRepository.list();
-  return res.json(all);
-});
+categoriesRoutes.post("/", createCategoryController.handle);
+categoriesRoutes.get("/", listCategoriesController.handle);
 
 export { categoriesRoutes };
