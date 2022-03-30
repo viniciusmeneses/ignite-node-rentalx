53939b32ad3a71de677bdcd1a671e8f9dbe73af4
Vinicius Meneses <vinicius.meneses@tagview.com.br>
2022-03-29 22:50:07 -0300

    Configura dataSource corretamente

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,11 +2,14 @@ import { DataSource } from "typeorm";
 
 const dataSource = new DataSource({
   type: "postgres",
-  host: "database",
+  host: "host.docker.internal",
   port: 5432,
   username: "docker",
   password: "ignite",
   database: "rentalx",
+  migrations: ["./src/database/migrations/*.ts"],
 });
 
 dataSource.initialize();
+
+export { dataSource };
