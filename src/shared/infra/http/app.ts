131f4dd14f1ebd954276a7d3e9836089f0db1c4a
Vinicius Meneses <vinicius.meneses@tagview.com.br>
2022-04-14 21:59:46 -0300

    Configura sentry

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -6,6 +6,8 @@ import "express-async-errors";
 import "dotenv/config";
 
 import upload from "@config/upload";
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
 import { AppError } from "@shared/errors/AppError";
 
 import swaggerFile from "../../../swagger.json";
@@ -16,15 +18,37 @@ import "../../container";
 
 const app = express();
 
-app.use(rateLimiter);
 app.use(cors());
+app.use(rateLimiter);
 app.use(express.json());
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Tracing.Integrations.Express({ app }),
+  ],
+  tracesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
 
 app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
 app.use(router);
+
+app.use(
+  Sentry.Handlers.errorHandler({
+    shouldHandleError(error) {
+      return error.status === 429 || error.status === 500;
+    },
+  })
+);
+
+app.use(Sentry.Handlers.errorHandler());
 
 app.use((err: Error, req: Request, res: Response, _next: NextFunction) => {
   if (err instanceof AppError)
