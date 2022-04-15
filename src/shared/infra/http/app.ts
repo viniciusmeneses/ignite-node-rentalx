4e0d6669e0083cb34671d952764bdc21512c049e
Vinicius Meneses <vinicius.meneses@tagview.com.br>
2022-04-14 21:21:30 -0300

    Configura CORS

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,3 +1,4 @@
+import cors from "cors";
 import express, { NextFunction, Request, Response } from "express";
 import swaggerUI from "swagger-ui-express";
 import "reflect-metadata";
@@ -14,6 +15,7 @@ import "../../container";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
